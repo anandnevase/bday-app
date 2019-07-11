d3b708def4b5b149f1e45e9529356c77329dc732
anandnevase <nevase.anand@gmail.com>
2019-07-11 14:12:33 +0530

    Added test cases

--- inspec-test.rb
+++ inspec-test.rb
@@ -7,7 +7,6 @@ control 'Birthday App testing - PUT Request' do
                 headers: {'Content-Type' => 'application/json'},
                 data: '{"dateOfBirth": "2018-07-08"}') do
     its('status') { should cmp 204 }
-    #its('body') { should eq 'username successfully updated!' }
   end
 
   describe http('http://localhost:7777/hello/anand124',
@@ -23,6 +22,18 @@ control 'Birthday App testing - PUT Request' do
     headers: {'Content-Type' => 'application/json'},
     data: '{"dateOfBirth": "2018/07/08"}') do
     its('status') { should cmp 400 }
-    #its('body') { should eq "{'message':'Username shoud contains only letters'}" }
+  end
+end
+
+control 'Birthday App testing - GET Request' do
+  title 'Automation testing'
+  desc  'TEST Bday App - Automation testing'
+
+  describe http('http://localhost:7777/hello/anand') do
+    its('status') { should cmp 200 }
+  end
+
+  describe http('http://localhost:7777/hello/anand124') do
+      its('status') { should cmp 400 }
   end
 end
